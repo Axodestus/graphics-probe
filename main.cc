204eb858841c1a2865a11e0582b53301231ff6e5
Axodestus <axodestus@yandex.ru>
2023-10-06 00:12:10 +0300

    Execution things.

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include <curl/curl.h>
 #include <sys/utsname.h>
 
--- probe/main.cc
+++ probe/main.cc
@@ -9,6 +9,10 @@ auto lam = [](std::string str) {
 
 int main() {
 
+    if (int temp = 10; temp > 5) {
+        std::cout << "temporary if-scope variable works! = " << temp << std::endl;
+    }
+
     std::cout << typeid(lam).name() << std::endl;
     std::cout << lam("hello probe") << std::endl;
     return 0;
